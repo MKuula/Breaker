fc31bd3f7d61e81b152ac6df0b370cdb3cad731f
Markus <Markus@DESKTOP-00FFVSH>
2024-05-02 00:46:47 +0300

    Add project files.

---
+++ Breaker/HiddenForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Breaker
+{
+    partial class HiddenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.isActiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpTimer = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1800000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.isActiveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(159, 48);
+            // 
+            // inOperationToolStripMenuItem
+            // 
+            this.isActiveToolStripMenuItem.Checked = true;
+            this.isActiveToolStripMenuItem.CheckOnClick = true;
+            this.isActiveToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isActiveToolStripMenuItem.Name = "inOperationToolStripMenuItem";
+            this.isActiveToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.isActiveToolStripMenuItem.Text = "Breaker is active";
+            this.isActiveToolStripMenuItem.Click += new System.EventHandler(this.breakerIsActiveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpTimer
+            // 
+            this.helpTimer.Enabled = true;
+            this.helpTimer.Interval = 60000;
+            this.helpTimer.Tick += new System.EventHandler(this.helpTimer_Tick);
+            // 
+            // HiddenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "HiddenForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.HiddenForm_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem isActiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer helpTimer;
+    }
+}
+
---
+++ Breaker/HiddenForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Breaker
+{
+    public partial class HiddenForm : Form
+    {
+        private bool rapidMessage = false;
+        private int ticksNum = 0;   // Spent time at the computer (after last arrival)
+
+        [DllImport("user32.dll")]
+        private static extern void SetParent(IntPtr window, int value);
+
+        private const int HWND_MESSAGE = -3;
+
+        public HiddenForm()
+        {
+            InitializeComponent();
+            SetParent(this.Handle, HWND_MESSAGE);
+        }
+
+        private void HiddenForm_Load(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = Properties.Resources.Reminder;
+            Microsoft.Win32.SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+            UpdateNotificationToolTipText();
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            if (e.Reason == Microsoft.Win32.SessionSwitchReason.SessionLock || e.Reason == Microsoft.Win32.SessionSwitchReason.SessionUnlock)
+            {
+                timer.Stop();
+                helpTimer.Stop();
+                timer.Interval = 1800000;
+
+                ticksNum = 0;   // Reset the number about the time spent at the computer
+                UpdateNotificationToolTipText();
+
+                // Next time show a normal message
+                rapidMessage = false;
+            }
+
+            if (e.Reason == Microsoft.Win32.SessionSwitchReason.SessionUnlock)
+            {
+                timer.Start();
+                helpTimer.Start();
+            }
+
+            isActiveToolStripMenuItem.Checked = timer.Enabled;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (!rapidMessage)
+                notifyIcon.ShowBalloonTip(10000, "Take a break", "It is the time to take a break. Please leave your computer now.", ToolTipIcon.Info);
+            else
+                notifyIcon.ShowBalloonTip(10000, "Take a break", "Keep the break as soon as possible!", ToolTipIcon.Warning);
+
+            rapidMessage = true;
+            timer.Interval = 600000;
+            timer.Start();
+        }
+
+        private void breakerIsActiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = isActiveToolStripMenuItem.Checked;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void helpTimer_Tick(object sender, EventArgs e)
+        {
+            ticksNum++;
+            UpdateNotificationToolTipText();
+            helpTimer.Start();
+        }
+
+        private void UpdateNotificationToolTipText()
+        {
+            if (ticksNum != 1)
+                notifyIcon.Text = string.Format("You have spent {0} minutes at the computer", ticksNum);
+            else
+                notifyIcon.Text = string.Format("You have spent {0} minute at the computer", ticksNum);
+        }
+    }
+}
